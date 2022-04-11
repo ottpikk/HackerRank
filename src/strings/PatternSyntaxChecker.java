721f1edadaa09a724474e38b98cb6e226e1c4ac2
Ott Pikk <ott@karmai.eu>
2022-04-11 09:38:06 +0300

    Pattern syntax checker

---
+++ src/strings/PatternSyntaxChecker.java
@@ -0,0 +1,25 @@
+package strings;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class PatternSyntaxChecker {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            testCases--;
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e){
+                System.out.println("Invalid");
+            }
+
+            //Write your code
+        }
+    }
+}
